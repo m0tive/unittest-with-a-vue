810194b904cd1ca80b46ce27a931732e0c939a84
Peter Dodds <peter@pddds.com>
2017-03-24 19:12:31 +0000

    Added unit tests for App humanizeUrl

--- test/unit/App.spec.js
+++ test/unit/App.spec.js
@@ -3,6 +3,18 @@ import App from 'App.vue'
 
 describe('App', () => {
     it('has a humanizeURL method', () => {
-        expect(typeof App.methods.humanizeURL).toBe('function')
+        expect(typeof App.methods.humanizeURL).toBe('function');
+    })
+
+    it('can remove http from urls', () => {
+        expect(App.methods.humanizeURL('http://google.com')).toBe('google.com');
+    })
+
+    it('can remove https from urls', () => {
+        expect(App.methods.humanizeURL('https://google.com')).toBe('google.com');
+    })
+
+    it('can remove trailing slash from urls', () => {
+        expect(App.methods.humanizeURL('google.com/')).toBe('google.com');
     })
 })
