8b64b7c6ffa30f236f1262caa3de96e49ed89927
Peter Dodds <peter@pddds.com>
2017-03-25 19:34:15 +0000

    Fetch tweets button

--- src/components/TwitterFeed.vue
+++ src/components/TwitterFeed.vue
@@ -1,17 +1,35 @@
 <template>
-    <ol class="twitter-feed">
-        <li>Feed goes heeeeere</li>
-    </ol>
+    <div class="twitter-feed">
+        <button v-on:click="getTweets">Get Tweets</button>
+        <ol class="twitter-feed" v-for="tweetObj in tweets">
+            <li>{{ tweetObj.tweet }}</li>
+        </ol>
+    </div>
 </template>
 
 <script>
-import { twitterFetcher } from 'twitterFetcher';
+import twitterFetcher from 'exports-loader?twitterFetcher!twitterFetcher';
+
 export default {
     name: 'twitter-feed',
-    data () {
+    data() {
         return {
-            userID: "pdddddds"
+            userID: "pdddddds",
+            tweets: [],
         }
     },
+    methods: {
+        getTweets() {
+            var vm = this;
+            twitterFetcher.fetch({
+                "profile": {"screenName": this.userID},
+                "dataOnly": true,
+                "showImages": true,
+                "customCallback": function(tweets) {
+                    vm.tweets = tweets;
+                },
+            });
+        }
+    }
 }
 </script>
